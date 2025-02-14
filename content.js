4e94cea7645a51d51c91e60fab7325cc381ea06c
Anzelah <jepkoechanzellah@gmail.com>
2025-02-14 18:52:04 +0300

    popup now shows. Needed to remove ads then inject a content script to run our ui

--- content.js
+++ content.js
@@ -1,18 +1,24 @@
 // CREATE A CHATBOT CONTAINER, THEN INJECT CHATBOT ONCE ADS ARE BLOCKED
 
 function injectChatbot() {
-    const matches = document.querySelectorAll("div.ad, [id*=ad], [class*=ad]")
+    //const matches = document.querySelectorAll("div.ad, [id*=ad], [class*=ad]")
+    const matches = document.querySelectorAll("div[id*='ad'], [id*='ad'], [class*='id]")
+    //const matches = document.querySelector("div[id^='div-gpt-ad-']") //working
+    //'[id*=.ad.]'
+    console.log(matches)
+    console.log('here')
 
     if (document.getElementById('chatbot-container')) return; // prevent more than one container loads
 
     const chatbotDiv = document.createElement("div");
     chatbotDiv.id = 'chatbot-container';
-    
+
     const iframeSrc = chrome.runtime.getURL('popup.html');
     chatbotDiv.innerHTML = `<iframe src="${iframeSrc}"></iframe>`; //load chatbot html/ui
 
-    if (matches.length > 0) {
-        matches[0].appendChild(chatbotDiv)
+    for (i = 0; i < matches.length; i++) {
+        const chatbotClone = chatbotDiv.cloneNode(true);
+        matches[i].replaceWith(chatbotClone)
     }
 }
 
