04cfabfb313c3dd59d5ff2e14d411b82430b9e0b
Anzelah <jepkoechanzellah@gmail.com>
2025-02-14 18:53:12 +0300

    first version done. Still need to perfect frontend and other type sof ads

--- content.js
+++ content.js
@@ -1,13 +1,7 @@
 // CREATE A CHATBOT CONTAINER, THEN INJECT CHATBOT ONCE ADS ARE BLOCKED
 
 function injectChatbot() {
-    //const matches = document.querySelectorAll("div.ad, [id*=ad], [class*=ad]")
     const matches = document.querySelectorAll("div[id*='ad'], [id*='ad'], [class*='id]")
-    //const matches = document.querySelector("div[id^='div-gpt-ad-']") //working
-    //'[id*=.ad.]'
-    console.log(matches)
-    console.log('here')
-
     if (document.getElementById('chatbot-container')) return; // prevent more than one container loads
 
     const chatbotDiv = document.createElement("div");
